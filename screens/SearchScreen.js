b31b64fafe694031901f5f0c41be7ebe6e41fd0b
karanwadhwa <wadhwa.karan96@gmail.com>
2019-07-17 20:48:01 +0530

    completed SearchScreen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import { TextInput, StyleSheet } from "react-native";
-import { View, Text } from "@shoutem/ui";
+import {
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator
+} from "react-native";
+import { View, Text, Title, Subtitle, Divider } from "@shoutem/ui";
 
-import { setSearchTerm } from "../store/actions/search";
+import { setSearchTerm, fetchSearchResults } from "../store/actions/search";
+import LongList from "../components/LongList";
 
 class SearchScreen extends React.Component {
   static navigationOptions = {
@@ -11,8 +19,74 @@ class SearchScreen extends React.Component {
     header: null
   };
 
+  renderRecents = () => {
+    return (
+      <View>
+        <Text style={{ color: "#FFF", fontSize: 28, paddingVertical: 15 }}>
+          Recent Searches
+        </Text>
+        <FlatList
+          data={this.props.search.recents}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => this.props.setSearchTerm(item)}>
+              <Text
+                style={{
+                  color: "#FFF",
+                  fontSize: 20,
+                  paddingVertical: 5
+                }}
+              >
+                {item}
+              </Text>
+              <Divider styleName="line" />
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  };
+
+  renderLoading = () => {
+    return (
+      <ActivityIndicator size="large" color="#FFF" style={styles.center} />
+    );
+  };
+
+  renderResults = () => {
+    return (
+      <ScrollView>
+        <LongList
+          data={this.props.search.searchResults}
+          navigation={this.props.navigation}
+        />
+      </ScrollView>
+    );
+  };
+
+  renderEmpty = searchTerm => {
+    return (
+      <View style={styles.center}>
+        <Title style={{ textAlign: "center", color: "#FFF" }}>
+          Oops!{"\n"}We couldnt find anything matching "{searchTerm}"
+        </Title>
+      </View>
+    );
+  };
+
+  renderBody = search => {
+    if (search.loading) return this.renderLoading();
+
+    if (search.searchTerm !== search.recents[0]) return this.renderRecents();
+
+    if (!search.searchResults.length) {
+      return this.renderEmpty(search.searchTerm);
+    }
+
+    return this.renderResults();
+  };
   render() {
-    const { search, setSearchTerm } = this.props;
+    const { search, setSearchTerm, fetchSearchResults } = this.props;
     return (
       <View style={styles.container}>
         <View style={styles.searchBarContainer}>
@@ -21,9 +95,10 @@ class SearchScreen extends React.Component {
             style={styles.searchBar}
             value={search.searchTerm}
             onChangeText={searchTerm => setSearchTerm(searchTerm)}
-            onEndEditing={() => alert(search.searchTerm)}
+            onEndEditing={() => fetchSearchResults(search.searchTerm)}
           />
         </View>
+        {this.renderBody(search)}
       </View>
     );
   }
@@ -42,6 +117,12 @@ const styles = StyleSheet.create({
     padding: 10,
     color: "#FFF",
     borderRadius: 3
+  },
+  center: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: "center",
+    width: "85%"
   }
 });
 
@@ -53,5 +134,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { setSearchTerm }
+  { setSearchTerm, fetchSearchResults }
 )(SearchScreen);
